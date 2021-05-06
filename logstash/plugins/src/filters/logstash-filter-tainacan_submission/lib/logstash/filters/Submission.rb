243a81ddbada7b5f1e928b8eab7a466dbfd3e8bb
Joycitta <joycitta@gmail.com>
2021-05-06 10:33:14 -0300

    update Submission with logs

--- logstash/plugins/src/filters/logstash-filter-tainacan_submission/lib/logstash/filters/Submission.rb
+++ logstash/plugins/src/filters/logstash-filter-tainacan_submission/lib/logstash/filters/Submission.rb
@@ -15,6 +15,8 @@ class Submission < PollProcess
     end
 
     def submission(item)
+      self.writeLog
+      begin
         if @url && @collectionId
           uriadress = "#{@url}/wp-json/tainacan/v2/collection/#{@collectionId}/items/submission"
           uri = URI.parse(uriadress)
@@ -36,24 +38,35 @@ class Submission < PollProcess
             http = Net::HTTP.new(uri.host, uri.port)
             request = Net::HTTP::Post.new(uri.request_uri, header)
             request.body = body.to_json
-            response = http.request(request)
-            response_obj = JSON.parse(response.body)
-            
+            response = http.request(request) 
+
             if response.is_a?(Net::HTTPSuccess)
-              response_obj = JSON.parse(response.body)
-              uri = URI.parse(@url + '/wp-json/tainacan/v2/collection/' + @collectionId + '/items/submission/' + response_obj['id'].to_s + '/finish')
-              http = Net::HTTP.new(uri.host, uri.port)
-              request = Net::HTTP::Post.new(uri.request_uri, header)
-              request.body = {}.to_json
-              response = http.request(request)
-              if response.is_a?(Net::HTTPSuccess)
                 response_obj = JSON.parse(response.body)
-                @id = response_obj['id']
-                return response_obj
-              end
+                uri = URI.parse(@url + '/wp-json/tainacan/v2/collection/' + @collectionId + '/items/submission/' + response_obj['id'].to_s + '/finish') 
+                http = Net::HTTP.new(uri.host, uri.port)
+                request = Net::HTTP::Post.new(uri.request_uri, header)
+                request.body = {}.to_json
+                response = http.request(request)
+                if response.is_a?(Net::HTTPSuccess)
+                  response_obj = JSON.parse(response.body)
+                  @id = response_obj['id']
+                  return response_obj
+                end
+            else 
+              response_obj = JSON.parse(response.body)
+              $log.error "Requisição errada #{response_obj}"
             end
           end
         end
+      rescue Exception => e
+        $log.error "Houve um erro!: #{e}"
+        return
+      end
+    end
+    
+    def writeLog
+       $log = Logger.new('writeLog.log', 'monthly')
+       $log.level = Logger::INFO
     end
 
     def exec
